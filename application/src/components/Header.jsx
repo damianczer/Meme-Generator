55797e9878300695b8917b8590416cb84db27d64
Damian Czerwiński <kontakt@damianczerwinski.pl>
2025-03-28 21:10:44 +0100

    create header & footer components & webpack configuration
    added header & footer components & webpack with plugins

---
+++ application/src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+const Header = () => {
+    return (
+        <header className="header">
+            <div className="logo">
+                <p>Meme Generator</p>
+            </div>
+
+        </header>
+    );
+}
+
+export default Header;
+
